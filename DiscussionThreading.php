b9da4203f8fdc93ee2ed3788f7d693f347391b24
Reedy <reedy@wikimedia.org>
2016-11-26 19:08:46 +0000

    Convert DiscussionThreading to extension registration
    Bump 1.6.0
    
    Change-Id: I0c51aba7aba8cdda7fd8fbb2ce2ddd37b64040cb

--- DiscussionThreading.php
+++ DiscussionThreading.php
@@ -1,41 +1,14 @@
 <?php
-/**
- * Extension to provide discussion threading similar to a listserv archive
- *
- * @file
- * @author Jack D. Pond <jack.pond@psitex.com>
- * @ingroup Extensions
- * @copyright  2007 Jack D. pond
- * @url http://www.mediawiki.org/wiki/Manual:Extensions
- * @licence GNU General Public Licence 2.0 or later
- */
-
-if (!defined('MEDIAWIKI')) die('Not an entry point.');
-
-# Internationalisation file
-$wgMessagesDirs['DiscussionThreading'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['DiscussionThreading'] =  __DIR__ . '/DiscussionThreading.i18n.php';
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'DiscussionThreading',
-	'author' => array( 'Jack D. Pond' , 'Daniel Brice' ),
-	'version' => '1.5.0',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:DiscussionThreading',
-	'descriptionmsg' => 'discussionthreading-desc',
-);
-
-/**
- * Set up hooks for discussion threading
- *
- * @param $wgSectionThreadingOn bool global logical variable to activate threading
- */
-global $wgSectionThreadingOn;
-$wgSectionThreadingOn = true;
-
-$wgAutoloadClasses['DiscussionThreading'] = __DIR__ . '/DiscussionThreading.class.php';
-
-$wgHooks['EditPage::showEditForm:initial'][] = 'DiscussionThreading::efDiscussionThread';
-$wgHooks['EditPage::attemptSave'][] = 'DiscussionThreading::onAttemptSave';
-$wgHooks['EditPage::showEditForm:initial'][] = 'DiscussionThreading::efDiscussionThreadEdit';
-$wgHooks['AlternateEdit'][] = 'DiscussionThreading::efDiscussionThreadEdit';
-$wgHooks['DoEditSectionLink'][] = 'DiscussionThreading::onDoEditSectionLink';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'DiscussionThreading' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['DiscussionThreading'] = __DIR__ . '/i18n';
+	/*wfWarn(
+		'Deprecated PHP entry point used for DiscussionThreading extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);*/
+	return;
+} else {
+	die( 'This version of the DiscussionThreading extension requires MediaWiki 1.28+' );
+}
